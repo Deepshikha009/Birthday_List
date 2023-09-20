4ab828b860a569008017a5d7e9239ee0b2e70cbe
Deepshikha <dkarkaulia09@gmail.com>
2023-09-20 20:25:06 +0530

    first commit

---
+++ src/compo/Obj.js
@@ -0,0 +1,344 @@
+const jsonData =[
+  {
+    "_id": 0,
+    "age": 22,
+    "name": "Hollie Terry",
+    "gender": "female",
+    "email": "hollieterry@vinch.com",
+    "day": 6,
+    "month": 1,
+    "year": 2012,
+    "phone": "+1(931) 409-3058"
+  },
+  {
+    "_id": 1,
+    "age": 30,
+    "name": "Deanne Odom",
+    "gender": "female",
+    "email": "deanneodom@vinch.com",
+    "day": 11,
+    "month": 8,
+    "year": 2005,
+    "phone": "+1(938) 596-3478"
+  },
+  {
+    "_id": 2,
+    "age": 35,
+    "name": "Avila Cochran",
+    "gender": "male",
+    "email": "avilacochran@vinch.com",
+    "day": 26,
+    "month": 12,
+    "year": 1991,
+    "phone": "+1(912) 445-2573"
+  },
+  {
+    "_id": 3,
+    "age": 24,
+    "name": "Trevino Clark",
+    "gender": "male",
+    "email": "trevinoclark@vinch.com",
+    "day": 17,
+    "month": 11,
+    "year": 2013,
+    "phone": "+1(898) 502-3311"
+  },
+  {
+    "_id": 4,
+    "age": 32,
+    "name": "Carmella Hammond",
+    "gender": "female",
+    "email": "carmellahammond@vinch.com",
+    "day": 16,
+    "month": 3,
+    "year": 1999,
+    "phone": "+1(940) 527-3664"
+  },
+  {
+    "_id": 5,
+    "age": 24,
+    "name": "Marshall Gill",
+    "gender": "male",
+    "email": "marshallgill@vinch.com",
+    "day": 10,
+    "month": 7,
+    "year": 1996,
+    "phone": "+1(888) 572-3255"
+  },
+  {
+    "_id": 6,
+    "age": 25,
+    "name": "Horton Dunlap",
+    "gender": "male",
+    "email": "hortondunlap@vinch.com",
+    "day": 14,
+    "month": 3,
+    "year": 2017,
+    "phone": "+1(845) 586-3019"
+  },
+  {
+    "_id": 7,
+    "age": 30,
+    "name": "Cathy Petty",
+    "gender": "female",
+    "email": "cathypetty@vinch.com",
+    "day": 19,
+    "month": 1,
+    "year": 2018,
+    "phone": "+1(962) 453-3693"
+  },
+  {
+    "_id": 8,
+    "age": 34,
+    "name": "Coleman Knapp",
+    "gender": "male",
+    "email": "colemanknapp@vinch.com",
+    "day": 10,
+    "month": 9,
+    "year": 2003,
+    "phone": "+1(981) 510-3040"
+  },
+  {
+    "_id": 9,
+    "age": 25,
+    "name": "Martina Murphy",
+    "gender": "female",
+    "email": "martinamurphy@vinch.com",
+    "day": 6,
+    "month": 2,
+    "year": 2006,
+    "phone": "+1(904) 600-2748"
+  },
+  {
+    "_id": 10,
+    "age": 36,
+    "name": "Pam Keith",
+    "gender": "female",
+    "email": "pamkeith@vinch.com",
+    "day": 24,
+    "month": 1,
+    "year": 2013,
+    "phone": "+1(950) 589-3209"
+  },
+  {
+    "_id": 11,
+    "age": 32,
+    "name": "Petra Becker",
+    "gender": "female",
+    "email": "petrabecker@vinch.com",
+    "day": 11,
+    "month": 5,
+    "year": 2011,
+    "phone": "+1(800) 440-3694"
+  },
+  {
+    "_id": 12,
+    "age": 30,
+    "name": "Fitzpatrick Sharp",
+    "gender": "male",
+    "email": "fitzpatricksharp@vinch.com",
+    "day": 8,
+    "month": 1,
+    "year": 2011,
+    "phone": "+1(883) 523-3608"
+  },
+  {
+    "_id": 13,
+    "age": 31,
+    "name": "Downs Powers",
+    "gender": "male",
+    "email": "downspowers@vinch.com",
+    "day": 28,
+    "month": 6,
+    "year": 2013,
+    "phone": "+1(914) 437-3752"
+  },
+  {
+    "_id": 14,
+    "age": 29,
+    "name": "Lucinda Avila",
+    "gender": "female",
+    "email": "lucindaavila@vinch.com",
+    "day": 11,
+    "month": 1,
+    "year": 2011,
+    "phone": "+1(983) 436-2312"
+  },
+  {
+    "_id": 15,
+    "age": 27,
+    "name": "Paula Kramer",
+    "gender": "female",
+    "email": "paulakramer@vinch.com",
+    "day": 10,
+    "month": 10,
+    "year": 2012,
+    "phone": "+1(855) 592-3143"
+  },
+  {
+    "_id": 16,
+    "age": 22,
+    "name": "Bernadine Morton",
+    "gender": "female",
+    "email": "bernadinemorton@vinch.com",
+    "day": 16,
+    "month": 10,
+    "year": 2021,
+    "phone": "+1(812) 568-3554"
+  },
+  {
+    "_id": 17,
+    "age": 23,
+    "name": "Stacie Parrish",
+    "gender": "female",
+    "email": "stacieparrish@vinch.com",
+    "day": 2,
+    "month": 4,
+    "year": 1998,
+    "phone": "+1(806) 491-3004"
+  },
+  {
+    "_id": 18,
+    "age": 22,
+    "name": "Jones Mcdowell",
+    "gender": "male",
+    "email": "jonesmcdowell@vinch.com",
+    "day": 7,
+    "month": 1,
+    "year": 2009,
+    "phone": "+1(996) 585-3525"
+  },
+  {
+    "_id": 19,
+    "age": 39,
+    "name": "Karla Simpson",
+    "gender": "female",
+    "email": "karlasimpson@vinch.com",
+    "day": 14,
+    "month": 4,
+    "year": 2010,
+    "phone": "+1(959) 518-2080"
+  },
+  {
+    "_id": 20,
+    "age": 21,
+    "name": "Shaffer Cantrell",
+    "gender": "male",
+    "email": "shaffercantrell@vinch.com",
+    "day": 20,
+    "month": 9,
+    "year": 1995,
+    "phone": "+1(816) 514-3146"
+  },
+  {
+    "_id": 21,
+    "age": 31,
+    "name": "Moore Ratliff",
+    "gender": "male",
+    "email": "mooreratliff@vinch.com",
+    "day": 28,
+    "month": 1,
+    "year": 2021,
+    "phone": "+1(962) 560-2886"
+  },
+  {
+    "_id": 22,
+    "age": 28,
+    "name": "Whitaker Short",
+    "gender": "male",
+    "email": "whitakershort@vinch.com",
+    "day": 12,
+    "month": 8,
+    "year": 2009,
+    "phone": "+1(863) 404-2901"
+  },
+  {
+    "_id": 23,
+    "age": 33,
+    "name": "Hurley Decker",
+    "gender": "male",
+    "email": "hurleydecker@vinch.com",
+    "day": 12,
+    "month": 7,
+    "year": 2017,
+    "phone": "+1(933) 496-3494"
+  },
+  {
+    "_id": 24,
+    "age": 40,
+    "name": "Wong Gates",
+    "gender": "male",
+    "email": "wonggates@vinch.com",
+    "day": 12,
+    "month": 1,
+    "year": 2014,
+    "phone": "+1(895) 504-2470"
+  },
+  {
+    "_id": 25,
+    "age": 40,
+    "name": "Sosa Garrison",
+    "gender": "male",
+    "email": "sosagarrison@vinch.com",
+    "day": 1,
+    "month": 11,
+    "year": 2013,
+    "phone": "+1(803) 507-2164"
+  },
+  {
+    "_id": 26,
+    "age": 21,
+    "name": "Dyer Garner",
+    "gender": "male",
+    "email": "dyergarner@vinch.com",
+    "day": 26,
+    "month": 3,
+    "year": 1995,
+    "phone": "+1(931) 533-3915"
+  },
+  {
+    "_id": 27,
+    "age": 39,
+    "name": "Olson Acosta",
+    "gender": "male",
+    "email": "olsonacosta@vinch.com",
+    "day": 22,
+    "month": 3,
+    "year": 2010,
+    "phone": "+1(916) 573-2345"
+  },
+  {
+    "_id": 28,
+    "age": 29,
+    "name": "Rebekah Boyd",
+    "gender": "female",
+    "email": "rebekahboyd@vinch.com",
+    "day": 6,
+    "month": 12,
+    "year": 2005,
+    "phone": "+1(955) 430-3119"
+  },
+  {
+    "_id": 29,
+    "age": 36,
+    "name": "Meghan Sharpe",
+    "gender": "female",
+    "email": "meghansharpe@vinch.com",
+    "day": 7,
+    "month": 12,
+    "year": 1994,
+    "phone": "+1(827) 421-3256"
+  }
+];
+
+// Function to convert JSON to URL-encoded string
+function jsonToUrl(jsonData) {
+  return Object.keys(jsonData[0]).map(key => {
+    return encodeURIComponent(key) + '=' + encodeURIComponent(jsonData[0][key]);
+  }).join('&');
+}
+
+// Call the function with your JSON data
+const urlEncodedData = jsonToUrl(jsonData);
+
+console.log(urlEncodedData);
